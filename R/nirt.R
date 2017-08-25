dd9e7e20dad22f356ea2de0fbe08f0b9cf2cdc77
danheck <danheck@web.de>
2017-08-25 17:50:45 +0200

    NIRT

--- R/nirt.R
+++ R/nirt.R
@@ -18,16 +18,16 @@
 #'
 #' The axioms are as follows:
 #' \itemize{
-#'   \item{\code{"WI1"}}{Weak row/subject independence: Persons can be ordered on
+#'   \item{\code{"W1"}}{Weak row/subject independence: Persons can be ordered on
 #'                       an ordinal scale independent of items.}
-#'   \item{\code{"WI2"}}{Weak column/item independence: Items can be ordered on
+#'   \item{\code{"W2"}}{Weak column/item independence: Items can be ordered on
 #'                       an ordinal scale independent of persons}
 #'   \item{\code{"DC"}}{Double cancellation: A necessary condition for a joint
 #'                      ordering of (person,item) pairs and an additive representation
 #'                      (i.e., an interval scale).}
 #' }
 #'
-#' Note that axioms WI1 and WI2 jointly define the ISOP model by Scheiblechner
+#' Note that axioms W1 and W2 jointly define the ISOP model by Scheiblechner
 #' (1995; isotonic ordinal probabilistic model) and the double homogeneity model
 #' by Mokken (1971). If DC is added, we obtain the ADISOP model
 #' by Scheiblechner (1999; ).
@@ -41,40 +41,75 @@
 #' @template ref_scheiblechner1995
 #' @template ref_scheiblechner1999
 #' @export
-nirt_to_Ab <- function(N, M, options = 2, axioms = c("WI1", "WI2")){
+nirt_to_Ab <- function(N, M, options = 2, axioms = c("W1", "W2")){
 
   idx <- outer(paste0("p", 1:N),
                paste0("i", 1:M), paste, sep = ",")
   A <- matrix(0, 0, M*N, dimnames=list(NULL, idx))
 
   # weak row independence (participants ordered): (N-1)*M constraints
-  if ("WI1" %in% axioms){
-    WI1_tmp <- diag(1, N-1, N) - cbind(0, diag(N-1))
+  if ("W1" %in% axioms){
+    W1_tmp <- diag(1, N-1, N) - cbind(0, diag(N-1))
     for (m in 1:M){
-      WI1 <- cbind(matrix(0, N-1, (m-1)*N), WI1_tmp, matrix(0, N-1, (M-m)*N))
-      rownames(WI1) <- rep("WI1", nrow(WI1))
-      A <- rbind(A, WI1 = WI1)
+      W1 <- cbind(matrix(0, N-1, (m-1)*N), W1_tmp, matrix(0, N-1, (M-m)*N))
+      rownames(W1) <- rep("W1", nrow(W1))
+      A <- rbind(A, W1 = W1)
     }
   }
 
   # weak column independence (participants ordered): N*(M-1) constraints
-  if ("WI2" %in% axioms){
+  if ("W2" %in% axioms){
     for (m in 1:(M-1)){
       for (n in 1:N){
         tmp <-  rep(0, ncol(A))
         tmp[(m-1)*N + c(n, n + N)] <- c(1, -1)
-        A <- rbind(A, "WI2" = tmp)
+        A <- rbind(A, "W2" = tmp)
       }
     }
+  }
+  b <- rep(0, nrow(A))
+  pt <- list("A" = A, "b" = b)
+
+  if ("DC" %in% axioms){
+    if (M < 3 | N < 3)
+      stop("To test double cancellation (DC), at least a 3x3 table is required.")
+    warning("Not yet implemented. Requires unions of convex polytopes.")
+
+    # for a 3x3 table:
+    # p12 < p21 & p23 < p32  =>  p13 < p31
+    # <=> not(p12 < p21 & p23 < p32  & not(p13 < p31) )
+    # <=> not(p12 < p21 & p23 < p32  & p13 > p31)
+    #          4     2     8     6      7     3   [idx]
+    DC.template <- matrix(c(0, -1, 0, 1, 0, 0, 0, 0, 0,
+                            0, 0, 1, 0, 0, 0, -1, 0, 0,
+                            0, 0, 0, 0, 0, -1, 0, 1, 0), 3, 9, byrow = TRUE)
+    # similarly: p12 > p21 & p23 > p32  =>  p13 > p31
+    #            => opposite: DC2 <- - DC1
+
+    cols <- matrix(combn(M, 3), 3)
+    rows <- matrix(combn(N, 3), 3)
+    exclude <- vector("list", 2 * choose(M, 3)*choose(N,3))  # number of 3x3 tables
+    cnt <- 1
+    indices <- matrix(1:(N*M), N)   # indices in Ab representation
+    for(j in 1:ncol(cols)){
+      for(i in 1:ncol(rows)){
+        A.tmp <- matrix(0, 3, M*N)
+        A.idx <- indices[rows[,i], cols[,j]]
+        A.tmp[,c(A.idx)] <- DC.template
+        exclude[[cnt]]$A <- A.tmp
+        exclude[[cnt + 1]]$A <- - A.tmp
+        exclude[[cnt]]$b <- exclude[[cnt + 1]]$b <- rep(0, 3)
+        cnt <- cnt + 2
+      }
+    }
+    pt$exclude <- exclude
+    # pt$b.exclude <- replicate(length(A.exclude), rep(0, 3), simplify = FALSE)
   }
 
   ################################ polytomous:
   # data: I x J x K contingency table
   # (unique persons, unique items, response categories)
   # possible: I <= N  , j<= M
-
-
-  b <- rep(0, nrow(A))
-  list("A" = A, "b" = b)
+  pt
 }
 
